c6994cdfcb5743f4225e7e9f3806270cfe975d6e
gokulstevee <65470058+gokulstevee@users.noreply.github.com>
2020-08-13 00:02:24 +0530

    script_modified-to_functions

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 const cards = document.querySelectorAll(".memory-card");
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
-let a = false
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
   this.classList.add("flip");
-  
+
   if (!hasFlippedCard) {
+    // first click
     hasFlippedCard = true;
     firstCard = this;
-  } else {
-    hasFlippedCard = false;
-    secondCard = this;
-    //is match
-    if (firstCard.dataset.framework == secondCard.dataset.framework) {
-      firstCard.removeEventListener("click", flipCard);
-      secondCard.removeEventListener("click", flipCard);
-    }
-    else{
-      setTimeout(() => {
-      firstCard.classList.remove('flip')
-      secondCard.classList.remove('flip')
-      }, 700);  
-    }
+    return;
   }
+  // second click
+  hasFlippedCard = false;
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMAtch = firstCard.dataset.framework == secondCard.dataset.framework;
+  isMAtch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    lockBoard = false;
+  }, 700);
 }
 
 cards.forEach((card) => {
-    card.addEventListener("click", flipCard)
+  card.addEventListener("click", flipCard);
 });
